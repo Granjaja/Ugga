86c79aeccffe3e58b58df2e4aaa9d41646a2f9a9
Gran <granvilekaranja@gmail.com>
2025-10-27 16:03:31 +0300

    cloudfare module removed

--- Cloud Test/frontend/next.config.ts
+++ Cloud Test/frontend/next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+// import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-// export default defineCloudflareConfig({
-// });
 
