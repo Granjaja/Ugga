d9dedf0e7b40f8ce4a843ce1619d57443dae0f5c
Gran <granvilekaranja@gmail.com>
2025-10-29 00:18:01 +0300

    nav enhanced

--- Cloud Test/frontend/src/app/components/Nav.tsx
+++ Cloud Test/frontend/src/app/components/Nav.tsx
@@ -11,25 +11,25 @@ function Nav() {
   const {data: session} = useSession();
 
   return (
-    <nav className="flex items-center m-2 p-2 bg-white rounded-md shadow-sm">
-        <div className='p-2 m-2'>
+    <nav className="flex items-center m-2 bg-white rounded-md shadow-sm">
+        <div className='m-2'>
           <Link href={"/"}>
         <h2 className=" flex items-center justify-center text-xl font-bold bg-gradient-to-r from-black via-blue-500 to-green-500 text-orange-500 p-2 rounded-md"> Ugga</h2>
         </Link>
         </div>
         <div className='flex items-center justify-center w-full'>
-          <div className='m-2 p-2'>
+          <div className='p-2'>
         <h3 className='cursor-pointer hover:underline hover:text-orange-400 font-bold'>
           <Link href={"/"}>Home</Link>
           </h3>
       </div>
       <div className='flex-grow text-center'>
-        <h3 className="text-lg font-medium m-2 md:inline-block">
+        <h3 className="font-medium m-2 md:inline-block">
                 {session ? (
                     <>
                     Welcome, <span className="text-orange-400">{session?.user?.name}</span>
                     </>
-                ) : ( "Welcome to Ugga")}
+                ) : ( "Welcome")}
 
       </h3>
       </div>
--- Cloud Test/frontend/src/app/layout.tsx
+++ Cloud Test/frontend/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       
 
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased m-5`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased m-3`}
       >
         <Providers>
             <Nav />
